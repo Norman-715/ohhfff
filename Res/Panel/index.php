4e08ec477995f8a8719574254d518cc4560aa642
toastytoad_xd <65022312+Norman-715@users.noreply.github.com>
2021-10-04 00:49:59 +0200

    Update index.php

--- Res/Panel/index.php
+++ Res/Panel/index.php
@@ -4,7 +4,7 @@ $site_name        = "HUGO PUBLIC VPS";
 $site_description = "Premium VPN/SSH/SSL Accounts Fast And Reliable Servers";
 $site_template    = "flatly"; // (flatly, darkly, sketchy, lumen, materia)
 $site_ip_address = "xxxxxxxxx"; // set site ip
-$site_paypal = "https://paypal.me/badboy897"; // set site paypal donate link
+$site_paypal = "https://paypal.me/callmehugo1"; // set site paypal donate link
 $site_contact = "https://t.me/hugo_pmbot"; // set site contact us link
 $daily_limit_user = "50"; // set daily limit
 
